791809719418d203d1921cfb4acc956313635987
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-07-12 17:20:02 -0700

    build api endpoint

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { fetchAllPokemon } from './util/api_util';
+import {receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import configureStore from "./store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
+
+window.requestAllPokemon = requestAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
